661354240498b6f402ea55c3d8644b250833324d
DaisyFragoso <fragosodaisy05@gmail.com>
2025-11-19 12:23:50 -0800

    added controls
    Added the following: move left, right, forward, backwards (AWSD); jump (space button); look around using mouse

---
+++ Prototype 4/Assets/Scripts/InputManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerInput PlayerInput;
+    private PlayerInput.OnFootActions onFoot;
+
+    private PlayerMotor motor;
+    private PlayerLook look;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        PlayerInput = new PlayerInput();
+        onFoot = PlayerInput.OnFoot;
+         
+        motor = GetComponent<PlayerMotor>();
+        look = GetComponent<PlayerLook>();
+        onFoot.Jump.performed += ctx => motor.Jump();
+    }
+
+    void FixedUpdate(){
+        //tells playermotor to move using the value from movement action 
+        motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
+    }
+     void LateUpdate(){
+        look.ProcessLook(onFoot.Look.ReadValue<Vector2>());
+    }
+    // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+    private void OnEnable(){
+        onFoot.Enable();
+    }
+    private void onDisable(){
+        onFoot.Disable();
+    }
+}
