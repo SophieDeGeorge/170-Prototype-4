b4c9a35c7bc5760169866abfcaae5c9b53939dfe
SophieDeGeorge <sophiedegeorge142@gmail.com>
2025-11-19 18:50:04 -0800

    StateHandler Outline

--- Prototype 4/Assets/Scripts/EnemyStateHandler.cs
+++ Prototype 4/Assets/Scripts/EnemyStateHandler.cs
@@ -1,19 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyStateHandler : MonoBehaviour
 {
-
-    //Pathfinding States
-        //Searching
-        //Wandering
-        //Chasing
+////////////////////////////////////////////////////////////////////////////////////////////////////////
         
-    //Territory States
-        //Mad (level gets scary and hostile)
-        //Neutral (not scary and hostile)
-        //More?
+        //Enum Declaration
 
+    public enum AIState 
+    {
+        Idle,
+        Patrol,
+        Chase
+    }
 
+    public enum TerritoryState 
+    {
+        Neutral,
+        Warning,
+        Danger
+    }
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+        
+        //State Object Types
+
+    AIState enemyState;
+    TerritoryState territoryState;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+        
+        //State Switch Case Functions
+
+    void DetermineAction()
+    {
+        switch (enemyState)
+        {
+            case AIState.Idle:
+            //idle
+            break;
+            case AIState.Patrol:
+            //patrol
+            break;
+            case AIState.Chase:
+            //chase
+            break;
+        }   
+    }
+
+    void DetermineTerritory()
+    {
+        switch (enemyState)
+        {
+            case TerritoryState.Neutral:
+            //neutral
+            break;
+            case TerritoryState.Warning:
+                //warning
+            break;
+            case TerritoryState.Danger:
+            //danger
+            break;
+        }  
+    }
 }
