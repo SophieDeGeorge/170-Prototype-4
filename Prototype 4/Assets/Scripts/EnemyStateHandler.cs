a0067494e4f8f3ef90f33575746cbedcaae5ff48
SophieDeGeorge <sophiedegeorge142@gmail.com>
2025-11-19 22:30:12 -0800

    Enemy View Range + Broken StateHandler

--- Prototype 4/Assets/Scripts/EnemyStateHandler.cs
+++ Prototype 4/Assets/Scripts/EnemyStateHandler.cs
@@ -5,6 +5,18 @@ using UnityEngine.AI;
 
 public class EnemyStateHandler : MonoBehaviour
 {
+
+
+    [SerializeField] private GameObject player;
+    [SerializeField] private NavMeshAgent agent;
+    private Vector3 patrolArea;
+    LayerMask layerMask;
+
+    void Awake()
+    {
+        layerMask = LayerMask.GetMask("Wall", "Player");
+    }
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
         
         //Enum Declaration
@@ -32,36 +44,67 @@ public class EnemyStateHandler : MonoBehaviour
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
         
         //State Switch Case Functions
-
+/*
     void DetermineAction()
     {
         switch (enemyState)
         {
             case AIState.Idle:
-            //idle
+                //idle
+                //pick random location on map as direction, walk there, look around
+            DetermineTerritory();
             break;
             case AIState.Patrol:
-            //patrol
+                //patrol
+                //pick random area in a radius around the players last seen location, go there, look around, after set time return to idle
+                patrolArea = agent.destination;
+                // search radius around patrolArea
+            DetermineTerritory(Warning);
             break;
             case AIState.Chase:
-            //chase
+                //chase
+                //run straight to player as long as player is seen
+                while (Physics.Raycast(transform.position, (player.GetComponent<Transform>().position - transform.position, 100f, layerMask))) //raycast hits player
+                {
+                    agent.destination = player.GetComponent<Transform>().position;
+                    DetermineTerritory(Danger);
+                }
+                agent.destination = player.GetComponent<Transform>().position;
+                DetermineAction(Patrol);
+            
             break;
         }   
     }
 
     void DetermineTerritory()
     {
-        switch (enemyState)
+        switch (territoryState)
         {
             case TerritoryState.Neutral:
-            //neutral
+                //neutral
             break;
             case TerritoryState.Warning:
                 //warning
             break;
             case TerritoryState.Danger:
-            //danger
+                //danger
             break;
         }  
     }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            DetermineAction(Chase);
+            //agent.destination = other.transform.position;
+        }
+    }
+
+    void Update()
+    {
+        //DetermineAction;
+        //DetermineTerritory;
+    }
+*/
 }
