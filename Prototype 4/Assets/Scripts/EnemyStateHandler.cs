4c9365e235bffe3ee470a9388f9b9a10ed95f1e4
SophieDeGeorge <sophiedegeorge142@gmail.com>
2025-12-02 15:13:34 -0800

    Redid Enemy State Script

--- Prototype 4/Assets/Scripts/EnemyStateHandler.cs
+++ Prototype 4/Assets/Scripts/EnemyStateHandler.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -14,6 +16,13 @@ public class EnemyStateHandler : MonoBehaviour
     LayerMask layerMask;
     private Vector3 rayDir;
     [SerializeField] AIState defaultState;
+    private Stack<Vector3> patrolPoints = new Stack<Vector3>();
+    private int numPoints;
+    [SerializeField] private float patrolRange;
+    private bool playerIsSeen;
+    [SerializeField] private float chaseSpeed;
+    [SerializeField] private float idleSpeed;
+
 
     void Awake()
     {
@@ -24,6 +33,7 @@ public class EnemyStateHandler : MonoBehaviour
     void Start()
     {
         enemyState = defaultState;
+        playerIsSeen = false;
     }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -59,33 +69,27 @@ public class EnemyStateHandler : MonoBehaviour
         switch (enemyState)
         {
             case AIState.Idle:
-                //idle
-                //pick random location on map as direction, walk there, look around
+                agent.speed = idleSpeed;
+                //pick random point on map
                 Vector3 point;
-            if (RandomPoint(transform.position, idleRange, out point)) 
-            {
-                Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f); 
+                RandomPoint(transform.position, idleRange, out point);
+                //destination = point
                 agent.SetDestination(point);
-            }
-            DetermineTerritory();
+        
             break;
-            case AIState.Patrol:
-                //patrol
-                //pick random area in a radius around the players last seen location, go there, look around, after set time return to idle
-                patrolArea = agent.destination;
-                // search radius around patrolArea
-            DetermineTerritory();
-            break;
-            case AIState.Chase:
-                agent.destination = player.transform.position;
-                territoryState = TerritoryState.Danger;
-                agent.destination = player.transform.position;
-                enemyState = AIState.Patrol;
-                
+            case AIState.Patrol: //find the code that determines when patrol starts, setup patrolPoints[] there
+                Debug.Log("patrol");
+                agent.SetDestination(patrolPoints.Pop());
             
             break;
-        }   
+            case AIState.Chase:
+                Debug.Log("chase");
+                agent.speed = chaseSpeed;
+                agent.SetDestination(player.transform.position);
+            break;
+        }
     }
+
 
     void DetermineTerritory()
     {
@@ -105,37 +109,75 @@ public class EnemyStateHandler : MonoBehaviour
 
     void OnTriggerStay(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.CompareTag("Player"))
         {
-            UpdateEnemy();
-            //Debug.Log("Enemy Collider Triggered");
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, player.transform.position - transform.position, out hit, layerMask))
+            {
+                rayDir = (player.GetComponent<Transform>().position - transform.position).normalized;
+
+                if ((hit.transform.name == "Player") && (enemyState != AIState.Chase))
+                {
+                    playerIsSeen = true;
+                    enemyState = AIState.Chase;
+                    DetermineAction();
+                } else
+                {
+                    playerIsSeen = false;
+                }
+            }
+        } else
+        {
+            playerIsSeen = false;
         }
     }
 
     void UpdateEnemy()
     {
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, player.transform.position - transform.position, out hit, layerMask))
+        if (enemyState == AIState.Idle)
         {
-            rayDir = (player.GetComponent<Transform>().position - transform.position).normalized;
-            Debug.DrawRay(transform.position, rayDir * hit.distance, Color.yellow);
-            //Debug.DrawRay(transform.position, (player.GetComponent<Transform>().position - transform.position) * hit.distance, Color.yellow);
-            //Debug.Log(hit.transform.name);
-            
-            if (hit.transform.name == "Player")
+            if (agent.remainingDistance == 0)
             {
-                enemyState = AIState.Chase;
-                Debug.Log("Chase activated");
+                DetermineAction();
             }
         }
-        DetermineAction();
+        if (enemyState == AIState.Patrol)
+        {
+            if (agent.remainingDistance == 0)
+            {
+                if (patrolPoints.Count == 0)
+                {
+                    enemyState = AIState.Idle;
+                }
+                DetermineAction();
+            }
+        }
+        if (enemyState == AIState.Chase)
+        {
+            if (playerIsSeen)
+            {
+                agent.SetDestination(player.transform.position);
+            } else
+            {
+                enemyState = AIState.Patrol;
+                patrolPoints.Clear();
+                numPoints = UnityEngine.Random.Range(2, 6);
+                for (int i = 0; i < numPoints; i++)
+                {
+                    Vector3 point;
+                    RandomPoint(agent.destination, patrolRange, out point);
+                    patrolPoints.Push(point);
+                }
+                DetermineAction();
+            }
+        }
         
     }
 
     bool RandomPoint(Vector3 center, float idleRange, out Vector3 result)
     {
 
-        Vector3 randomPoint = center + Random.insideUnitSphere * idleRange; //random point in a sphere 
+        Vector3 randomPoint = center + UnityEngine.Random.insideUnitSphere * idleRange; //random point in a sphere 
         NavMeshHit hit;
         if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas)) //documentation: https://docs.unity3d.com/ScriptReference/AI.NavMesh.SamplePosition.html
         { 
@@ -155,12 +197,7 @@ public class EnemyStateHandler : MonoBehaviour
 
     void Update()
     {
-        if(agent.remainingDistance <= agent.stoppingDistance) //done with path
-        {
-            enemyState = AIState.Idle;
-            DetermineAction();
-            
-        }
+        UpdateEnemy();
     }
 
 }
